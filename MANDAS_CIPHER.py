23a1ddfb23c23a29803bda437431fc8903f44fb3
Tyler Dorsey <dorseytyler5@gmail.com>
2025-01-25 11:52:59 -0700

    updated

--- MANDAS_CIPHER.py
+++ MANDAS_CIPHER.py
@@ -64,22 +64,96 @@ A_Matrix = np.array([
 #--------------------------------#
 #----- ENCRYPTION ALGORITHM -----#
 #--------------------------------# 
+print(f'word array: {word_array}')
+print(f'key array:  {key_array}')
+
 
 cipher_array = []
 n = 0 
 
 for item in word_array: 
     if key_array[n] == 'U': 
-       if item == 'T': 
+       if item == 'A': 
            cipher_array.append('Z')
            n += 1 
        elif item == 'B': 
            cipher_array.append('Y')
            n += 1  
+       elif item == 'C': 
+           cipher_array.append('#') 
+           n += 1
+       elif item == 'D': 
+           cipher_array.append('%')
+           n += 1
+       elif item == 'E': 
+           cipher_array.append('&')
+           n += 1 
+       elif item == 'F': 
+           cipher_array.append('R')
+           n += 1
+       elif item == 'G': 
+           cipher_array.append('A')
+           n += 1 
+       elif item == 'H': 
+           cipher_array.append('B')
+           n += 1 
+       elif item == 'I': 
+           cipher_array.append('C')
+           n += 1 
+       elif item == 'J': 
+           cipher_array.append('D')
+           n += 1 
+       elif item == 'K': 
+           cipher_array.append('E')
+           n += 1 
+       elif item == 'L': 
+           cipher_array.append('S')
+           n += 1 
+       elif item == 'M': 
+           cipher_array.append('*') 
+           n += 1 
+       elif item == 'N': 
+           cipher_array.append('M')
+           n += 1 
+       elif item == 'O': 
+           cipher_array.append('G')
+           n += 1 
+       elif item == 'P': 
+           cipher_array.append('H')
+           n += 1 
+       elif item == 'Q': 
+           cipher_array.append('I')
+           n += 1 
+       elif item == 'R': 
+           cipher_array.append('J')
+           n += 1 
        elif item == 'S': 
-           cipher_array.append('YEAHMAAAAN')
+           cipher_array.append('K')
+           n += 1 
+       elif item == 'T': 
+           cipher_array.append('U')
+           n += 1 
+       elif item == 'U': 
+           cipher_array.append('N')
+           n += 1 
+       elif item == 'V': 
+           cipher_array.append('O')
+           n += 1 
+       elif item == 'W': 
+           cipher_array.append('P')
+           n += 1 
+       elif item == 'X': 
+           cipher_array.append('Q')
+           n += 1 
+       elif item == 'Y': 
+           cipher_array.append('W')
+           n += 1 
+       elif item == 'Z': 
+           cipher_array.append('V')                               
+        
     else: 
-        print('NOOOOO')
+        cipher_array.append('!')
+        n += 1
 print(cipher_array)       
 
 
