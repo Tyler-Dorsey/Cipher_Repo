db7f67d335f45b50a29d4c895d3b1643ed433778
Tyler Dorsey <dorseytyler5@gmail.com>
2025-02-06 23:35:34 -0700

    updated

--- EXTENDED_CEASAR.py
+++ EXTENDED_CEASAR.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 ####----------------------------------------------------------------------------####
 # Title: EXTENDED CEASAR CIPHER                                                    # 
 # Author: Tyler Dorsey                                                             #
-# Purpose: The ceasar cipher is one of the world's moldest ciphers. While it was a #
+# Purpose: The ceasar cipher is one of the world's oldest ciphers. While it was a  #
 #          success for it's day, it can be easily cracked. The extended Ceasar     #
 #          cipher aims at taking the original cipher and making it harder to crack #
 #          The product of two keys will be taken and each digit will be used to    #
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 
 
 # Phrase To Be Encrypted
-word = 'Tyler is great.'
+word = 'Tyler is great. He is the best person that I know.'
 word = word.upper()
 word = word.replace('.','--')
 word = word.replace(' ', '.')
@@ -32,10 +32,35 @@ print(f'Product of the keys: {product_key}')
 N = 13
 factor = 10**N 
 truncated_key = math.floor(factor * product_key) 
+truncated_key = [char for char in str(truncated_key)]
 print(f'Truncated Key: {truncated_key}') 
 
 #--- Alphabet Matrix ---# 
 alphabet_matrix = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+# Truncated Key Check To Word Length
+difference = abs(len(word)-len(truncated_key))
+
+while len(word) != len(truncated_key):
+    
+    if difference > len(truncated_key):
+        truncated_key += truncated_key 
+        difference = abs(len(word)-len(truncated_key))
+        
+    elif difference < len(truncated_key): 
+        truncated_key += truncated_key[:difference]
+        difference = abs(len(word)-len(truncated_key))
+        
+    elif difference == 0: 
+        break 
+print(len(truncated_key))
+
+
+if len(word) == len(truncated_key): 
+    print('----------------------------------')
+    print('SUCCESS: Word length and key length match')
+    print(f'Word Length: {len(word)}')
+    print(f'Key Length : {len(truncated_key)}')
 
 
 
